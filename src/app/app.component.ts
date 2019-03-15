e32b1646024912d116e75e2e089a5a764eb2020e
adam <adam.bjp@gmail.com>
2019-03-15 17:41:36 +0100

    adding button to inserting html tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, ElementRef, ViewChild, ViewEncapsulation} from '@angular/core';
 import * as HtmlValidator from 'html-tag-validator';
 
 @Component({
@@ -8,10 +8,27 @@ import * as HtmlValidator from 'html-tag-validator';
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
+  @ViewChild('textAreaElement') textAreaElement: ElementRef;
   htmlFormat = ''
   error: string = '';
 
   constructor() {
+  }
+
+  insertElement(el) {
+    console.log(this.textAreaElement.nativeElement.selectionStart);
+    console.log(this.textAreaElement.nativeElement.selectionEnd);
+
+    let a = this.htmlFormat;
+    let b = "<"+el+">";
+    let b2 = "</"+el+">";
+    let posStart = this.textAreaElement.nativeElement.selectionStart;
+    let posEnd = this.textAreaElement.nativeElement.selectionEnd;
+    if(posStart === posEnd) {
+      this.htmlFormat = [a.slice(0, posStart), b, b2, a.slice(posStart)].join('');
+    } else {
+      this.htmlFormat = [a.slice(0, posStart), b, a.slice(posStart, posEnd), b2, a.slice(posEnd)].join('');
+    }
   }
 
   typeTextarea(event) {
